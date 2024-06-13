b4d683dfa68db161dd6b8ed42bdff241844dd6de
Kartik Khare <110482774+Kartikkhare18@users.noreply.github.com>
2024-06-14 02:48:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
-  },
-};
-
 export default nextConfig;/** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
