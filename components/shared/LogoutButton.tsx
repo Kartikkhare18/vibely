9993ef2b61c913a108f25074c96b8dfa43020e8f
Kartik Khare <110482774+Kartikkhare18@users.noreply.github.com>
2024-06-14 02:26:44 +0530

    Update LogoutButton.tsx

--- components/shared/LogoutButton.tsx
+++ components/shared/LogoutButton.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { AiOutlineLogout } from "react-icons/ai";
 import { logoutHandler } from "@/lib/serveractions";
-import { IoMdLogOut } from "react-icons/io";
 
 const LogoutButton = () => {
   return (
     <form action={logoutHandler}>
       <Button size={"icon"} className="rounded-full">
-        <IoMdLogOut size="24px" />
+        <AiOutlineLogout size="24px" />
       </Button>
     </form>
   );
