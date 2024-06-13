5f07ff1d613734e43f943e8dfe1abf22252dd2b4
Kartik Khare <110482774+Kartikkhare18@users.noreply.github.com>
2024-06-14 02:25:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="shortcut icon"
-          href="@/public/favicon.ico"
-          type="image/x-icon"
-        />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
