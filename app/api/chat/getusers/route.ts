ff335bf35014e87e121a93b545566f753357e13a
Kartik Khare <110482774+Kartikkhare18@users.noreply.github.com>
2024-06-14 02:21:11 +0530

    Update route.ts

--- app/api/chat/getusers/route.ts
+++ app/api/chat/getusers/route.ts
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import connectDataBase from "@/lib/db";
+import connectDB from "@/lib/db";
 import { User, UserDocument } from "@/models/user.model";
 import { NextResponse } from "next/server";
 
@@ -7,10 +7,12 @@ export const GET = async () => {
   try {
     const authUser = await auth();
     if (!authUser) return;
-    await connectDataBase();
+    await connectDB();
+
     const users: UserDocument[] = await User.find({
       _id: { $ne: authUser?.user?._id },
     });
+
     return NextResponse.json(users);
   } catch (error) {
     console.log(error);
