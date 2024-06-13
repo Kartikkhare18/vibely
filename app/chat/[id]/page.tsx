2243e6572fa5b1b1ae52ce41875f1e64a2547ca2
Kartik Khare <110482774+Kartikkhare18@users.noreply.github.com>
2024-06-14 02:22:06 +0530

    Update page.tsx

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@/auth";
 import ChatPage from "@/components/ChatPage";
-import { getMessages, getprofileUser } from "@/lib/userdata";
+import { getMessages, getUserProfile } from "@/lib/userdata";
 import React from "react";
 
 const ChattingPage = async ({ params }: { params: { id: string } }) => {
-  let userProfile = await getprofileUser(params.id);
+  let userProfile = await getUserProfile(params.id);
+  userProfile = JSON.stringify(userProfile);
   const authUser = await auth();
   const messages = authUser
     ? await getMessages(authUser?.user?._id, params.id)
