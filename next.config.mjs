2847954f48579272547e670965bbde311a8c74f3
Kartik Khare <110482774+Kartikkhare18@users.noreply.github.com>
2024-06-14 02:41:06 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,4 +10,26 @@ const nextConfig = {
   },
 };
 
+export default nextConfig;/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'w7.pngwing.com'
+            },
+            {
+                protocol:'https',
+                hostname:'res.cloudinary.com'
+            },
+        ]
+    },
+    experimental:{
+        serverActions:{
+            bodySizeLimit:'2mb'
+        }
+    }
+};
+
 export default nextConfig;
+
